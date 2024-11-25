bb8e63e59ada80aeab602c029018839ceeb40a16
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-11-25 17:23:55 +0100

    Introduce skeletal Ouroboros node and associated simulation

---
+++ ouroboros-consensus/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod block;
+pub mod chain;
+pub mod crypto;
+pub mod event;
+pub mod ffi;
+pub mod message;
+pub mod network;
+pub mod ouroboros_node;
+
+/// For testing purpose, must be at the toplevel
+#[cfg(test)]
+extern crate quickcheck_macros;
